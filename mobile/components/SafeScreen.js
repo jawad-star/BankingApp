faa739fc7763e72fa4f5af85963556b713008ad4
jawad-star <jawwadsaleem913@gmail.com>
2025-07-17 17:24:39 +0500

    feat(auth): integrate Clerk for authentication with sign-in and sign-up flows
    - Added Clerk Expo SDK for user authentication.
    - Implemented sign-in and sign-up screens with email and password handling.
    - Created authentication layout to manage user sessions.
    - Added safe area component for consistent UI across devices.
    - Included sign-out functionality with redirection.
    - Updated package dependencies and TypeScript configuration.

---
+++ mobile/components/SafeScreen.js
@@ -0,0 +1,21 @@
+import { View, Text } from "react-native";
+import React from "react";
+import {COLORS} from "@constants/colors";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const SafeScreen = ({ children }) => {
+  const insets = useSafeAreaInsets();
+  return (
+    <View
+      style={{
+        paddingTop: insets.top,
+        flex: 1,
+        backgroundColor: COLORS.Primary,
+      }}
+    >
+      {children}
+    </View>
+  );
+};
+
+export default SafeScreen;
