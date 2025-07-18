313c93373149fbe398166e3c9da58c7b06b95147
jawad-star <jawwadsaleem913@gmail.com>
2025-07-18 11:47:34 +0500

    feat: enhance authentication and transaction management UI with new components and improved user experience

--- mobile/app/(auth)/_layout.jsx
+++ mobile/app/(auth)/_layout.jsx
@@ -1,12 +1,13 @@
-import { Redirect, Stack } from 'expo-router'
-import { useAuth } from '@clerk/clerk-expo'
+import { Redirect, Stack } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
 export default function AuthRoutesLayout() {
-  const { isSignedIn } = useAuth()
+  const { isSignedIn, isLoaded } = useAuth();
+  if (!isLoaded) return null;
 
   if (isSignedIn) {
-    return <Redirect href={'/'} />
+    return <Redirect href={"/"} />;
   }
 
-  return <Stack />
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
